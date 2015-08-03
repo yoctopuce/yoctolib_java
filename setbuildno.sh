5c7c2261981acf5c04122395351178215f4cb805
root <root@linux-build.(none)>
2015-08-03 12:44:48 +0200

    "New build based on revison 20983"

--- setbuildno.sh
+++ setbuildno.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "set build number to 1.10.20975-SNAPSHOT"
-mvn versions:set -DnewVersion=1.10.20975-SNAPSHOT -DgenerateBackupPoms=false
+echo "set build number to 1.10.20983-SNAPSHOT"
+mvn versions:set -DnewVersion=1.10.20983-SNAPSHOT -DgenerateBackupPoms=false
 cd yoctolib
-mvn versions:set -DnewVersion=1.10.20975-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=1.10.20983-SNAPSHOT -DgenerateBackupPoms=false
--- yoctolib/src/main/java/com/yoctopuce/YoctoAPI/YAPI.java
+++ yoctolib/src/main/java/com/yoctopuce/YoctoAPI/YAPI.java
@@ -60,7 +60,7 @@ public class YAPI
     public static final long INVALID_LONG = -9223372036854775807L;
     public static final int INVALID_UINT = -1;
     public static final String YOCTO_API_VERSION_STR = "1.10";
-    public static final String YOCTO_API_BUILD_STR = "20975";
+    public static final String YOCTO_API_BUILD_STR = "20983";
     public static final int YOCTO_API_VERSION_BCD = 0x0110;
     public static final int YOCTO_VENDORID = 0x24e0;
     public static final int YOCTO_DEVID_FACTORYBOOT = 1;
@@ -1221,7 +1221,7 @@ public class YAPI
      */
     public static String GetAPIVersion()
     {
-        return YOCTO_API_VERSION_STR + ".20975";
+        return YOCTO_API_VERSION_STR + ".20983";
     }
 
     /**
