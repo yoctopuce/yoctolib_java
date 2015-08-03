4f028717ae4c68100fbddd2d27343c27e13212ee
root <root@linux-build.(none)>
2015-08-03 09:22:42 +0200

    "New build based on revison 20975"

--- setbuildno.sh
+++ setbuildno.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "set build number to 1.10.20971-SNAPSHOT"
-mvn versions:set -DnewVersion=1.10.20971-SNAPSHOT -DgenerateBackupPoms=false
+echo "set build number to 1.10.20975-SNAPSHOT"
+mvn versions:set -DnewVersion=1.10.20975-SNAPSHOT -DgenerateBackupPoms=false
 cd yoctolib
-mvn versions:set -DnewVersion=1.10.20971-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=1.10.20975-SNAPSHOT -DgenerateBackupPoms=false
