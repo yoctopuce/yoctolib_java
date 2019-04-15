d7f53578bbd8268167c9574829b57350981454b8
Yoctopuce <dev@yoctopuce.com>
2019-04-15 15:25:51 +0200

    "New build based on revison 35153"

--- yoctolib-jEE/src/main/java/com/yoctopuce/YoctoAPI/YAPI.java
+++ yoctolib-jEE/src/main/java/com/yoctopuce/YoctoAPI/YAPI.java
@@ -58,7 +58,7 @@ public class YAPI
     public static final long INVALID_LONG = -9223372036854775807L;
     public static final int INVALID_UINT = -1;
     public static final String YOCTO_API_VERSION_STR = "1.10";
-    public static final String YOCTO_API_BUILD_STR = "35124";
+    public static final String YOCTO_API_BUILD_STR = "35153";
     public static final int YOCTO_API_VERSION_BCD = 0x0110;
     public static final int YOCTO_VENDORID = 0x24e0;
     public static final int YOCTO_DEVID_FACTORYBOOT = 1;
@@ -285,7 +285,7 @@ public class YAPI
      */
     public static String GetAPIVersion()
     {
-        return YOCTO_API_VERSION_STR + ".35124" + YUSBHub.getAPIVersion();
+        return YOCTO_API_VERSION_STR + ".35153" + YUSBHub.getAPIVersion();
     }
 
     /**
