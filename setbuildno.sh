9a7fec8ed5a762f47ecd2fd45052556219ffa041
Yoctopuce <dev@yoctopuce.com>
2019-06-05 08:41:34 +0200

    "New build based on revison 35652"

--- setbuildno.sh
+++ setbuildno.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "set build number to 1.10.35635-SNAPSHOT"
-mvn versions:set -DnewVersion=1.10.35635-SNAPSHOT -DgenerateBackupPoms=false
+echo "set build number to 1.10.35652-SNAPSHOT"
+mvn versions:set -DnewVersion=1.10.35652-SNAPSHOT -DgenerateBackupPoms=false
 cd yoctolib
-mvn versions:set -DnewVersion=1.10.35635-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=1.10.35652-SNAPSHOT -DgenerateBackupPoms=false
 cd ../yoctolib-jEE
-mvn versions:set -DnewVersion=1.10.35635-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=1.10.35652-SNAPSHOT -DgenerateBackupPoms=false
