12d3ae75034b9049ee7db64b2e93fd45591bdf80
Yoctopuce <dev@yoctopuce.com>
2026-06-12 12:33:02 +0200

    "New build based on revision 74699"

--- setbuildno.sh
+++ setbuildno.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "set build number to 2.1.14544-SNAPSHOT"
-mvn versions:set -DnewVersion=2.1.14544-SNAPSHOT -DgenerateBackupPoms=false
+echo "set build number to 2.1.14699-SNAPSHOT"
+mvn versions:set -DnewVersion=2.1.14699-SNAPSHOT -DgenerateBackupPoms=false
 cd yoctolib
-mvn versions:set -DnewVersion=2.1.14544-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=2.1.14699-SNAPSHOT -DgenerateBackupPoms=false
 cd ../yoctolib-jEE
-mvn versions:set -DnewVersion=2.1.14544-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=2.1.14699-SNAPSHOT -DgenerateBackupPoms=false
